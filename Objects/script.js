8e87e5d46992f5cbd36e84c3e3c6da1a33883687
Arjun9603 <arjunraghav774@gmail.com>
2025-10-03 22:50:01 +0530

    Added Objects in JS

---
+++ Objects/script.js
@@ -0,0 +1,59 @@
+// const car={
+//     name:"BMW",
+//     topSpeed:200,
+//     frun(){
+//         console.log("car is running at 200km/h");
+//     }
+// }
+
+
+// const person={
+//     name:"Arjun",
+//     age:20,
+//     eyecolor:"black",
+//     greet(){
+//         console.log(`Hello ${this.name}`);
+        
+//     }
+// }
+// person.greet();
+
+
+// const vehicle={}
+// vehicle.name="rolls Royce";
+// vehicle.topSpeed=250;
+// vehicle.color="black";
+// vehicle.run=function(){
+//     console.log(`car is running at ${this.topSpeed}km/h`);
+// }
+
+// // let ans=vehicle;
+// // // console.log(vehicle);
+// vehicle.run();
+
+
+
+// Create an Object
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+
+// // person.firstName
+// let text=person;
+
+
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+const mySister = new Person("Anna", "Rally", 18, "green");
+console.log(myFather);
+// console.log(myMother);
+// console.log(mySister);
+
+
+// Create a Person constructor
+ 
+
+
